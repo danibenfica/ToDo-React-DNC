952e44001f50280d57f766e8344fbf3beead623c
Daniele Benfica <103818625+danibenfica@users.noreply.github.com>
2023-07-28 15:30:55 -0300

    criação do componente ThemeContext

---
+++ ThemeContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isLightMode, setIsLightMode] = useState(true);
+
+  const toggleTheme = () => {
+    setIsLightMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isLightMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
